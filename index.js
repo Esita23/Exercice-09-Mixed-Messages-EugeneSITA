2ae6a05709446fab3bd5618e0f729782ae9a229e
Esita23 <135968759+Esita23@users.noreply.github.com>
2023-08-03 08:11:29 +0000

    rassemblement des élément

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// eugene sita
+
+
+function genererMessageAleatoire() {
+    // Initialisation des valeurs
+    const messages = [
+        "Quand j'étais petit à la maison, le plus dur c'était la fin du mois",
+        "Bonjour vous etes chinois",
+        "Attendez-vous à rencontrer une personne spéciale aujourdhui",
+        "Les articles définis en français sont : le, la, les.",
+        "Les articles indéfinis en français sont : un, une, des.",
+        "Votre journée sera remplie d'énergie positive.",
+        "Attendez-vous à rencontrer une personne spéciale aujourd'hui.",
+        "Il est temps d'explorer de nouvelles opportunités.",
+    ];
+
+    // Générer le message
+    const messageAleatoire = messages[Math.floor(Math.random() * messages.length)];
+    console.log(messageAleatoire);
+}
+
+genererMessageAleatoire();
